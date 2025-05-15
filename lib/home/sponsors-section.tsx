50cdf162ec02e36f2faf005ef935cac4d326fa20
Whisperpiano <jesusalberola90@gmail.com>
2025-05-15 14:32:14 +0200

    feat(home): enhance sponsors section with interactive links and improved layout for better user engagement

--- lib/home/sponsors-section.tsx
+++ lib/home/sponsors-section.tsx
@@ -6,34 +6,124 @@ import Sponsor5 from "@/lib/assets/sponsors/logoipsum-5.svg";
 import Sponsor6 from "@/lib/assets/sponsors/logoipsum-6.svg";
 import Sponsor7 from "@/lib/assets/sponsors/logoipsum-7.svg";
 import Sponsor8 from "@/lib/assets/sponsors/logoipsum-8.svg";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function SponsorsSection() {
   return (
     <article className="grid grid-cols-4">
-      <div className="border aspect-[16/6] flex items-center justify-center">
-        <Sponsor1 className="w-32 h-auto " />
-      </div>
-      <div className="border aspect-[16/6] flex items-center justify-center">
-        <Sponsor2 className="w-32 h-auto " />
-      </div>
-      <div className="border aspect-[16/6] flex items-center justify-center">
-        <Sponsor3 className="w-32 h-auto " />
-      </div>
-      <div className="border aspect-[16/6] flex items-center justify-center">
-        <Sponsor4 className="w-32 h-auto " />
-      </div>
-      <div className="border aspect-[16/6] flex items-center justify-center">
-        <Sponsor5 className="w-32 h-auto " />
-      </div>
-      <div className="border aspect-[16/6] flex items-center justify-center">
-        <Sponsor6 className="w-32 h-auto " />
-      </div>
-      <div className="border aspect-[16/6] flex items-center justify-center">
-        <Sponsor7 className="w-32 h-auto " />
-      </div>
-      <div className="border aspect-[16/6] flex items-center justify-center">
-        <Sponsor8 className="w-32 h-auto " />
-      </div>
+      <Link
+        className="group border aspect-[16/6] flex items-center justify-center relative p-4 "
+        href="https://logoipsum.com/"
+        target="_blank"
+      >
+        <div className="transition-all duration-200 [cubic-bezier(0.165, 0.84, 0.44, 1)] translate-y-0 group-hover:-translate-y-4 duration-300 flex items-center justify-center w-full h-full">
+          <Sponsor1 className="w-32 h-auto " />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-16 group-hover:translate-y-6 transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)]">
+          <span className="flex items-center gap-1.5 text-sm font-medium">
+            Learn More <ArrowRight size={16} />
+          </span>
+        </div>
+      </Link>
+      <Link
+        className="group border aspect-[16/6] flex items-center justify-center relative p-4 "
+        href="https://logoipsum.com/"
+        target="_blank"
+      >
+        <div className="transition-all duration-200 [cubic-bezier(0.165, 0.84, 0.44, 1)] translate-y-0 group-hover:-translate-y-4 duration-300 flex items-center justify-center w-full h-full">
+          <Sponsor2 className="w-32 h-auto " />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-16 group-hover:translate-y-6 transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)]">
+          <span className="flex items-center gap-1.5 text-sm font-medium">
+            Learn More <ArrowRight size={16} />
+          </span>
+        </div>
+      </Link>
+      <Link
+        className="group border aspect-[16/6] flex items-center justify-center relative p-4 "
+        href="https://logoipsum.com/"
+        target="_blank"
+      >
+        <div className="transition-all duration-200 [cubic-bezier(0.165, 0.84, 0.44, 1)] translate-y-0 group-hover:-translate-y-4 duration-300 flex items-center justify-center w-full h-full">
+          <Sponsor3 className="w-32 h-auto " />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-16 group-hover:translate-y-6 transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)]">
+          <span className="flex items-center gap-1.5 text-sm font-medium">
+            Learn More <ArrowRight size={16} />
+          </span>
+        </div>
+      </Link>
+      <Link
+        className="group border aspect-[16/6] flex items-center justify-center relative p-4 "
+        href="https://logoipsum.com/"
+        target="_blank"
+      >
+        <div className="transition-all duration-200 [cubic-bezier(0.165, 0.84, 0.44, 1)] translate-y-0 group-hover:-translate-y-4 duration-300 flex items-center justify-center w-full h-full">
+          <Sponsor4 className="w-32 h-auto " />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-16 group-hover:translate-y-6 transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)]">
+          <span className="flex items-center gap-1.5 text-sm font-medium">
+            Learn More <ArrowRight size={16} />
+          </span>
+        </div>
+      </Link>
+      <Link
+        className="group border aspect-[16/6] flex items-center justify-center relative p-4 "
+        href="https://logoipsum.com/"
+        target="_blank"
+      >
+        <div className="transition-all duration-200 [cubic-bezier(0.165, 0.84, 0.44, 1)] translate-y-0 group-hover:-translate-y-4 duration-300 flex items-center justify-center w-full h-full">
+          <Sponsor5 className="w-32 h-auto " />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-16 group-hover:translate-y-6 transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)]">
+          <span className="flex items-center gap-1.5 text-sm font-medium">
+            Learn More <ArrowRight size={16} />
+          </span>
+        </div>
+      </Link>
+      <Link
+        className="group border aspect-[16/6] flex items-center justify-center relative p-4 "
+        href="https://logoipsum.com/"
+        target="_blank"
+      >
+        <div className="transition-all duration-200 [cubic-bezier(0.165, 0.84, 0.44, 1)] translate-y-0 group-hover:-translate-y-4 duration-300 flex items-center justify-center w-full h-full">
+          <Sponsor6 className="w-32 h-auto " />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-16 group-hover:translate-y-6 transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)]">
+          <span className="flex items-center gap-1.5 text-sm font-medium">
+            Learn More <ArrowRight size={16} />
+          </span>
+        </div>
+      </Link>
+      <Link
+        className="group border aspect-[16/6] flex items-center justify-center relative p-4 "
+        href="https://logoipsum.com/"
+        target="_blank"
+      >
+        <div className="transition-all duration-200 [cubic-bezier(0.165, 0.84, 0.44, 1)] translate-y-0 group-hover:-translate-y-4 duration-300 flex items-center justify-center w-full h-full">
+          <Sponsor7 className="w-32 h-auto " />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-16 group-hover:translate-y-6 transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)]">
+          <span className="flex items-center gap-1.5 text-sm font-medium">
+            Learn More <ArrowRight size={16} />
+          </span>
+        </div>
+      </Link>
+      <Link
+        className="group border aspect-[16/6] flex items-center justify-center relative p-4 "
+        href="https://logoipsum.com/"
+        target="_blank"
+      >
+        <div className="transition-all duration-200 [cubic-bezier(0.165, 0.84, 0.44, 1)] translate-y-0 group-hover:-translate-y-4 duration-300 flex items-center justify-center w-full h-full">
+          <Sponsor8 className="w-32 h-auto " />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-16 group-hover:translate-y-6 transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)]">
+          <span className="flex items-center gap-1.5 text-sm font-medium">
+            Learn More <ArrowRight size={16} />
+          </span>
+        </div>
+      </Link>
     </article>
   );
 }
