2739b45f06b74a2295664f261ebddd8039c6b255
Whisperpiano <jesusalberola90@gmail.com>
2025-05-19 10:22:31 +0200

    feat: add tech stack icons and orbiting animation components

--- components/home/components/orbiting-icon.tsx
+++ components/home/components/orbiting-icon.tsx
@@ -1,7 +1,21 @@
-export function OrbitIcon({ children }: { children: React.ReactNode }) {
+import { cn } from "@/lib/utils";
+
+export interface OrbitIconProps {
+  children: React.ReactElement<React.SVGProps<SVGSVGElement>>;
+  darkColor: string;
+  lightColor: string;
+}
+
+export function OrbitIcon({ children, darkColor, lightColor }: OrbitIconProps) {
   return (
-    <div className="aspect-square size-11 rounded-full bg-muted-foreground/10 backdrop-blur shadow-md flex items-center justify-center border border-muted-foreground/10">
-      <div className="aspect-square size-6 flex items-center justify-center">
+    <div
+      className={cn(
+        "aspect-square size-11 rounded-full backdrop-blur shadow-md flex items-center justify-center border border-muted-foreground/10"
+      )}
+    >
+      <div
+        className={`aspect-square size-6 flex items-center justify-center ${darkColor} ${lightColor}`}
+      >
         {children}
       </div>
     </div>
--- components/home/utils/home-constants.ts
+++ components/home/utils/home-constants.ts
@@ -15,6 +15,17 @@ import Sponsor6 from "@/lib/assets/sponsors/logoipsum-6.svg";
 import Sponsor7 from "@/lib/assets/sponsors/logoipsum-7.svg";
 import Sponsor8 from "@/lib/assets/sponsors/logoipsum-8.svg";
 
+import ReactIcon from "@/lib/assets/icons/react.svg";
+import TypeScriptIcon from "@/lib/assets/icons/typescript.svg";
+import TailwindIcon from "@/lib/assets/icons/tailwindcss.svg";
+import ShadcnIcon from "@/lib/assets/icons/shadcn.svg";
+import FramerIcon from "@/lib/assets/icons/framer.svg";
+import AuthjsIcon from "@/lib/assets/icons/authjs.svg";
+import ZodIcon from "@/lib/assets/icons/zod.svg";
+import DrizzleIcon from "@/lib/assets/icons/drizzle.svg";
+import TursoIcon from "@/lib/assets/icons/turso.svg";
+import CloudinaryIcon from "@/lib/assets/icons/cloudinary.svg";
+
 import { SiGithub } from "react-icons/si";
 
 export const features = [
@@ -104,5 +115,74 @@ export const sponsors = [
     name: "Logo Ipsum",
     logo: Sponsor8,
     href: "https://logoipsum.com/",
+  },
+];
+
+export const outsideIcons = [
+  {
+    name: "Tailwind CSS",
+    icon: TailwindIcon,
+    dark: "dark:text-[#38bdf8]",
+    light: "text-[#38bdf8]",
+  },
+  {
+    name: "Framer Motion",
+    icon: FramerIcon,
+    dark: "dark:text-foreground",
+    light: "text-foreground",
+  },
+  {
+    name: "React",
+    icon: ReactIcon,
+    dark: "dark:text-[#58c4dc]",
+    light: "text-[#087ea4]",
+  },
+  {
+    name: "Shadcn UI",
+    icon: ShadcnIcon,
+    dark: "dark:text-foreground",
+    light: "text-foreground",
+  },
+];
+
+export const middleIcons = [
+  {
+    name: "TypeScript",
+    icon: TypeScriptIcon,
+    dark: "dark:text-[#3178C6]",
+    light: "text-[#3197c6]",
+  },
+  {
+    name: "AuthJS",
+    icon: AuthjsIcon,
+    dark: "dark:text-foreground",
+    light: "text-foreground",
+  },
+  {
+    name: "Zod",
+    icon: ZodIcon,
+    dark: "dark:text-[#3175d4]",
+    light: "text-[#45a6df]",
+  },
+];
+
+export const insideIcons = [
+  {
+    name: "Drizzle",
+    icon: DrizzleIcon,
+    dark: "dark:text-[#C5F74F]",
+    light: "text-[#7fa724]",
+  },
+  {
+    name: "Turso",
+    icon: TursoIcon,
+    dark: "dark:text-foreground",
+    light: "text-foreground",
+  },
+  {
+    name: "Cloudinary",
+    icon: CloudinaryIcon,
+    dark: "dark:text-[#3448C5]",
+    light: "text-[#3448C5]",
   },
 ];
