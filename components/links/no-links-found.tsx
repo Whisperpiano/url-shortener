c951f32772e3fa74a3174416e8ff8b2e3ccc901b
Whisperpiano <jesusalberola90@gmail.com>
2025-05-23 08:40:43 +0200

    feat: add empty state UI with animated link card skeleton and create button

--- components/links/no-links-found.tsx
+++ components/links/no-links-found.tsx
@@ -63,7 +63,7 @@ export default function NoLinksFound() {
       </div>
 
       <div className="pointer-events-none absolute inset-x-0 top-0 h-1/4 bg-gradient-to-b from-background"></div>
-      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-[80%] bg-gradient-to-t from-background"></div>
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-background"></div>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/6 bg-gradient-to-r from-background"></div>
       <div className="pointer-events-none absolute inset-y-0 right-0 w-1/6 bg-gradient-to-l from-background"></div>
 
